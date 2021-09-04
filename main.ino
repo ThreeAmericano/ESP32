2d8d1da6637e843112456134293d57db41b54b13
WSP99 <nosod3@naver.com>
2021-09-04 23:48:28 +0900

    Update main.ino

--- main.ino
+++ main.ino
@@ -2,12 +2,19 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include "DHT.h"
+#include <Adafruit_NeoPixel.h>
 
 // 가전 동작 상태
-char applianceStatus[10] = "018050000";
+String applianceStatus = "00000000";
 String receiveMsg = "018050000";
 
-#define GAS_VALVE 16
+#define GAS_LED 16
+#define GAS_VALVE 17
+#define VALVE_CNT 10 // 1초
+boolean gasLedStatus = LOW;
+volatile boolean gasValveStatus = false;
+boolean gasStatusFlag = false;
+volatile unsigned char gasCnt = 0;
 
 // Firebase
 // Your Firebase Project Web API Key
@@ -36,11 +43,7 @@ const char* mqtt_server = "211.179.42.130";
 const char* mqtt_user = "rabbit";
 const char* mqtt_pass= "MQ321";
 
-void callback(char* topic, byte* payload, unsigned int length) {
-//  Serial.print("Message arrived [");
-//  Serial.print(topic);
-//  Serial.print("] ");
-  
+void callback(char* topic, byte* payload, unsigned int length) {  
   if(length != 9) {
     Serial.println("WrongData Received");
   }
@@ -50,9 +53,25 @@ void callback(char* topic, byte* payload, unsigned int length) {
       for (int i = 0; i < length; i++) {
         receiveMsg +=(char)payload[i];
       }
-      Serial.print(receiveMsg);
-      Serial.println();
+      Serial.println(receiveMsg);
+
+      for(int i = 1; i < 9; i++) {
+          applianceStatus[i-1] = receiveMsg[i];
+        }
+        if(applianceStatus[0] == '0') ledcWrite(AIRCON_PWM_CHANNEL, 0);
+        if(applianceStatus[0] == '1'){
+            windPower = applianceStatus[1] - '0';
+            Serial.print("windPower : ");
+            Serial.println(windPower);
+            ledcWrite(AIRCON_PWM_CHANNEL, (windPower*26));
+        }
+
+        if(applianceStatus[7] == '0') digitalWrite(GAS_LED, LOW);
+        else digitalWrite(GAS_LED, HIGH);
+
+        Firebase.setString(firebaseData, "/smarthome/status", applianceStatus);
   }
+  
 }
 
 void setup_wifi() {
@@ -80,40 +99,57 @@ float temperature = 24.5;
 volatile char timerInterruptCnt = 0;
 volatile char applianceTimerCnt = 0;
 hw_timer_t *timer = NULL;
-#define TIMER_COUNT 5000000
+#define TIMER_COUNT 100000
 
 // 타이머 인터럽트 서비스 루틴
 void IRAM_ATTR onTimer() {
   // 타이머 카운트 변수 오버플로 방지
   if (timerInterruptCnt < 0xFF) timerInterruptCnt++;
-  if (applianceTimerCnt < 0xFF) applianceTimerCnt++;
+//  if (applianceTimerCnt < 0xFF) applianceTimerCnt++;
+  if (!(digitalRead(GAS_VALVE))){
+    if (gasCnt < 0xFF) gasCnt++;
+  }else gasCnt = 0;
 }
 
 // while에서 체크하여 시간마다 실행할 것 작성
 void chk_interrupt(){
-    // 10초마다 가전명령 수행
-    if(applianceTimerCnt >= 2) {
-        applianceTimerCnt = 0;
-        for(int i = 0; i < 9; i++) {
-          applianceStatus[i] = receiveMsg[i];
-        }
-        if(applianceStatus[1] == '0') ledcWrite(AIRCON_PWM_CHANNEL, 0);
-        if(applianceStatus[1] == '1'){
-            windPower = applianceStatus[2] - '0';
-            Serial.print("windPower : ");
-            Serial.println(windPower);
-            ledcWrite(AIRCON_PWM_CHANNEL, (windPower*26));
-        }
 
-        if(applianceStatus[8] == '0') digitalWrite(GAS_VALVE, LOW);
-        else digitalWrite(GAS_VALVE, HIGH); 
-        
+   // 버튼이 1초 이상 눌린 경우 LED toggle 실행
+    if(gasCnt >= 10) {
+      if(gasValveStatus == false) {
+          gasValveStatus = true;
+          gasLedStatus = gasLedStatus? LOW : HIGH;
+          applianceStatus[7] = gasLedStatus? '1' : '0';
+          Firebase.setString(firebaseData, "/smarthome/status", applianceStatus);
+          digitalWrite(GAS_LED, gasLedStatus);
+      }
+    }
+    else {
+      gasValveStatus = false;
     }
     
-    //if (timerInterruptCnt >= 12) {
-    if (timerInterruptCnt >= 6) {
+    // 10초마다 가전명령 수행
+//    if(applianceTimerCnt >= 100) {
+//        applianceTimerCnt = 0;
+//        for(int i = 0; i < 9; i++) {
+//          applianceStatus[i] = receiveMsg[i];
+//        }
+//        if(applianceStatus[1] == '0') ledcWrite(AIRCON_PWM_CHANNEL, 0);
+//        if(applianceStatus[1] == '1'){
+//            windPower = applianceStatus[2] - '0';
+//            Serial.print("windPower : ");
+//            Serial.println(windPower);
+//            ledcWrite(AIRCON_PWM_CHANNEL, (windPower*26));
+//        }
+//
+//        if(applianceStatus[8] == '0') digitalWrite(GAS_LED, LOW);
+//        else digitalWrite(GAS_LED, HIGH); 
+    
+    //}
+    
+    if (timerInterruptCnt >= 300) {
       Serial.print("Firebase Status : ");
-      Serial.println( Firebase.ready( ) );
+      Serial.println(Firebase.ready());
       timerInterruptCnt = 0;
       
       //습도 측정
@@ -137,6 +173,8 @@ void chk_interrupt(){
       Serial.print(humidity);
       Serial.print("Temperature: ");
       Serial.println(temperature);
+
+      
       }
 }
 
@@ -144,7 +182,7 @@ void interrupt_init() {
   //프리스케일러 80 (80MHz/80 = 1MHz)
   timer = timerBegin(0, 80, true);
   timerAttachInterrupt(timer, &onTimer, true);
-  // 카운트 수 오백만번 -> 5초 후 인터럽트 서비스 루틴 실행
+  // 카운트 수 십만번 -> 0.1초 후 인터럽트 서비스 루틴 실행
   timerAlarmWrite(timer, TIMER_COUNT, true);
   timerAlarmEnable(timer);
 }
@@ -157,7 +195,7 @@ void reconnect() {
     // Attempt to connect
     if (client.connect("PSW_Arduino", mqtt_user, mqtt_pass)) {
       Serial.println("connected");
-      client.subscribe("webos.smarthome");
+      client.subscribe("webos.smarthome.#");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -169,7 +207,8 @@ void reconnect() {
 
 void setup() {
   Serial.begin(115200);
-  pinMode(GAS_VALVE, OUTPUT);
+  pinMode(GAS_LED, OUTPUT);
+  pinMode(GAS_VALVE, INPUT);
   setup_wifi();
   Firebase.begin(DATABASE_URL, API_KEY);
   Firebase.reconnectWiFi(true);
@@ -178,7 +217,7 @@ void setup() {
   
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
-  Serial.println(client.subscribe("webos.smarthome"));
+  Serial.println(client.subscribe("webos.smarthome.#"));
   
   //ledcSetup(ch, freq, resolution)
   ledcSetup(AIRCON_PWM_CHANNEL, AIRCON_PWM_FREQUENCY, PWM_RESOLUTION);
